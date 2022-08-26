11b5c8f247e6facb9762c6d7567debcbce250612
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2022-08-26 16:23:17 +0300

    Added webpage to modify blocked websites
    Blocking and unblocking websites is possible now (althought they will change back to ["google.com","google.gr","www.example.com"] on every browser startup due to background script)

---
+++ blocked_websites/script.js
@@ -0,0 +1,130 @@
+let buttons = document.getElementsByTagName('button');
+for (let button of buttons) {
+	if (button.classList.contains("tabbutton")) {
+		button.addEventListener("click", openTab);
+	}
+	if (button.id == "block_button") {
+		button.addEventListener("click", blockWebsite);
+	}
+}
+loadTable();
+
+function isValidWebsiteName(website) {
+	// The function returning "true" doesn't mean the domain name entered is 100% correct
+	let domain_array = website.split(".").filter(function(entry) {return entry.trim() != '';});
+	return (
+		domain_array.length >= 2 &&
+		domain_array[domain_array.length-1].length >= 2
+	)
+}
+
+function blockWebsite(_evt) {
+	chrome.storage.local.get("blocked_websites", function(items) {
+		// Begin by getting the user input
+		let user_input = document.getElementById("input_url").value;
+		let blocked_websites = items.blocked_websites;
+		// Begin by check if user input is valid and if already in localStorage
+		if (isValidWebsiteName(user_input) && !blocked_websites.includes(user_input)) {
+			// If not, proceed
+			console.log("Website to block: "+user_input);
+			// Then, read the "blocked_websites" Array
+			if (typeof blocked_websites == "undefined") {
+				// If nothing is defined, make a new Array with the user_input
+				blocked_websites = [user_input]
+			} else {
+				// Else, push the user_input to the blocked_websites
+				blocked_websites.push(user_input);
+			}
+			console.log("New blocked websites: "+blocked_websites);
+			// Lastly, save them to localStorage and update the table
+			chrome.storage.local.set({
+				"blocked_websites": blocked_websites
+			});
+			loadTable();
+		} else {
+			// Else, print a message
+			console.log("Nothing to block");
+		}
+	});
+}
+
+function unblockWebsite(website) {
+	// Begin by reading the localStorage
+	chrome.storage.local.get("blocked_websites", function(items) {
+		// Code below obtained from https://stackoverflow.com/a/3954451/
+		// It basically removes "website" from "items" (if it exists)
+		let blocked_websites = items.blocked_websites;
+		let index = blocked_websites.indexOf(website);
+		if (index != -1) {
+			blocked_websites.splice(index, 1);
+			// Once removed "website" from "items". save it to localStorage
+			chrome.storage.local.set({
+				"blocked_websites": blocked_websites
+			});
+			// Lastly, update the table
+			loadTable();
+		}
+	});
+}
+
+function loadTable(_evt) {
+	chrome.storage.local.get("blocked_websites", function(items) {
+		let blocked_websites = typeof items.blocked_websites == "undefined" ? [] : items.blocked_websites;
+		if (blocked_websites) { // if array isn't empty, execute the rest of the code
+			// Create an empty table
+			let table = document.createElement("table");
+			// Edit it's ID
+			table.id = "blocked_websites_table";
+			// Iterate blocked_websites and add them to the table, one by one
+			for (let website of blocked_websites) {
+				// Create a new row
+				let row = table.insertRow();
+				// Create a cell which contains the blocked URL
+				let url_cell = row.insertCell();
+				url_cell.innerHTML = website;
+				// Create a cell with a small button to unblock the corresponding website
+				let unblock_cell = row.insertCell();
+				let unblock_button = document.createElement("button");
+				unblock_button.innerHTML = "Unblock";
+				unblock_button.addEventListener("click", function() {unblockWebsite(website)});
+				unblock_cell.appendChild(unblock_button);
+
+			}
+			// Once filled the table with the websites, append it to "table" division
+			let htmltablediv = document.getElementById("table");
+			htmltablediv.innerHTML = "Blocked websites:\n";
+			htmltablediv.appendChild(table);
+		} else {
+			let htmltablediv = document.getElementById("table");
+			htmltablediv.innerHTML = "Nothing is blocked yet. Try inputting a website";
+		}
+	});
+}
+
+function openTab(evt) {
+	let toshowTabId = evt.target.id.slice(7);
+	let toshowTab = document.getElementById(toshowTabId);
+
+	/*if (toshowTab) {*/
+		let visible_tabs = document.getElementsByClassName("visible_tab");
+		let button_is_active = evt.target.className.endsWith("active");
+		for (let visible_tab of visible_tabs) {
+			visible_tab.className = "hidden_tab";
+		}
+
+		let tabbuttons = document.getElementsByClassName("tabbutton");
+		for (let tabbutton of tabbuttons) {
+			tabbutton.className = tabbutton.className.replace(" active", "");
+		}
+
+		if (!button_is_active) {
+			toshowTab.className = "visible_tab";
+			evt.target.className += " active";
+		}
+	/*}*/
+	/*let hidden_tabs = document.getElementsByClassName("hidden_tab");
+	for (let i = 0; i < hidden_tabs.length; i++) {
+		hidden_tabs[i].className = "visible_tab";
+	}*/
+	//document.getElementById(tabName).className = "visible_tab";
+}
